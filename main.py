13675acb928524edcc44352efcab9591fdd6e318
Mathias Winkel <mathias.winkel@merckgroup.com>
2021-11-15 14:14:55 +0100

    disallow mixed products of found permutations

--- main.py
+++ main.py
@@ -12,13 +12,33 @@ def to_ndarray(v, m, n, dtype=int):
             result[i, j] = v[i, j].value
     return result
 
+
 def matshow(v):
     for row in v:
         line = '|'
         for col in row:
-            line += ' ' if col==0 else '#'
+            line += ' ' if col == 0 else '#'
         line += '|'
         print(line)
+
+
+def create_permutation_combination_constraints(m, id, n_nodes, fullprod, all_permutations, level, last_choice=-1, fullstr=''):
+    is_identity = np.allclose(fullprod, id)
+
+    if last_choice != -1 and is_identity:
+        print(f'Skipping Id = {fullstr}')
+    else:
+        if not is_identity:
+            print(f'Adding Constraint for {fullstr}')
+            m.knownPermutations.add(expr=sum(m.P[tuple(ij)] for ij in np.argwhere(fullprod)) <= n_nodes - 1)
+
+        if level >= 0:
+            for ip, permutation in enumerate(all_permutations):
+                if ip == last_choice:
+                    continue
+                for p, power in enumerate(permutation):
+                    create_permutation_combination_constraints(
+                        m, id, n_nodes, fullprod@power, all_permutations, level-1, ip, f'{fullstr} P_{ip}^{p+1}')
 
 
 def main():
@@ -37,7 +57,7 @@ def main():
     solver = po.SolverFactory('glpk')
 
     print('Creating Parameter for Concurrence Matrix')
-    model.A = po.Param(model.N, model.N, initialize=A) #, within=po.Reals)
+    model.A = po.Param(model.N, model.N, initialize=A)  # , within=po.Reals)
 
     print('Creating Boolean Permutation Matrix')
     model.P = po.Var(model.N, model.N, within=po.Boolean)
@@ -61,37 +81,41 @@ def main():
     print('Creating Constraint to Limit Negative Deviation')
     model.negDev = po.Constraint(model.N, model.N, rule=lambda m, i, j: -m.T[i, j] <= deviation(m, i, j))
 
+    print('Creating Constraint to Exclude Identity')
+    model.identityConstraint = po.Constraint(expr=sum(model.P[i, i] for i in range(n_nodes)) <= n_nodes - 1)
+
     model.knownPermutations = po.ConstraintList()
 
     id = np.eye(n_nodes)
-    known_result = id
 
     found_transformations = []
 
     for i_result in count(0):
+        print('Solving using glpk')
+        results = solver.solve(model, tee=True)
+        print(results)
+
+        permutation = to_ndarray(model.P, n_nodes, n_nodes)
+        print(f'P_{i_result} =')
+        matshow(permutation)
+
         print(f'Computing Cycle of Solution P_{i_result}')
         all_powers = []
-        power = known_result.copy()
+        power = permutation.copy()
         while True:
             all_powers.append(power)
-            power = power@known_result
+            power = power@permutation
             if np.allclose(power, id):
                 break
 
         found_transformations.append(all_powers)
 
-        print('Creating Constraints to Exclude Known Solution and its Powers')
-        for p, power in enumerate(all_powers):
-            print(f'P_{i_result}^{p}')
-            model.knownPermutations.add(expr=sum(model.P[tuple(ij)] for ij in np.argwhere(power)) <= n_nodes - 1)
-
-        print('Solving using glpk')
-        results = solver.solve(model, tee=True)
-        print(results)
-
-        known_result = to_ndarray(model.P, n_nodes, n_nodes)
-        print(f'P_{i_result + 1} =')
-        matshow(known_result)
+        print('Creating Constraints to Exclude Known Solutions, their Powers and their Combinatorial Products')
+        model.del_component(model.knownPermutations)
+        model.del_component(model.knownPermutations_index)
+        model.knownPermutations = po.ConstraintList()
+        create_permutation_combination_constraints(model, id, n_nodes, id, found_transformations, 4)
+        print(f'Created {len(model.knownPermutations)} constraints.')
 
     #print('Creating Solver using MindtPy')
     #solver = po.SolverFactory('mindtpy')
