39c1126fdfd72c4f8e7ed4f6d456ffe8e9fff25b
Mathias Winkel <mathias.winkel@merckgroup.com>
2021-11-15 13:15:18 +0100

    some cleanup, disallow previously found permutations

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pickle
+from itertools import count
 
 import numpy as np
 import pyomo.environ as po
@@ -10,6 +11,14 @@ def to_ndarray(v, m, n, dtype=int):
         for j in range(n):
             result[i, j] = v[i, j].value
     return result
+
+def matshow(v):
+    for row in v:
+        line = '|'
+        for col in row:
+            line += ' ' if col==0 else '#'
+        line += '|'
+        print(line)
 
 
 def main():
@@ -24,8 +33,11 @@ def main():
     model = po.ConcreteModel()
     model.N = po.Set(initialize=range(n_nodes))
 
+    print('Creating Solver using glpk')
+    solver = po.SolverFactory('glpk')
+
     print('Creating Parameter for Concurrence Matrix')
-    model.A = po.Param(model.N, model.N, initialize=A)
+    model.A = po.Param(model.N, model.N, initialize=A) #, within=po.Reals)
 
     print('Creating Boolean Permutation Matrix')
     model.P = po.Var(model.N, model.N, within=po.Boolean)
@@ -38,6 +50,9 @@ def main():
     print('Creating Column Sum Constraint for the Permutation Matrix')
     model.colSum = po.Constraint(model.N, rule=lambda m, j: 1 == sum(m.P[i, j] for i in m.N))
 
+    print('Creating Objective Function')
+    model.objective = po.Objective(expr=sum(model.T[i, j] for j in model.N for i in model.N), sense=po.minimize)
+
     def deviation(m, i, j):
         return sum(m.P[i, k]*A[k, j] for k in m.N) - sum(A[i, k]*m.P[k, j] for k in m.N)
 
@@ -46,22 +61,42 @@ def main():
     print('Creating Constraint to Limit Negative Deviation')
     model.negDev = po.Constraint(model.N, model.N, rule=lambda m, i, j: -m.T[i, j] <= deviation(m, i, j))
 
-    print('Creating Objective Function')
-    model.objective = po.Objective(expr=sum(model.T[i, j] for j in model.N for i in model.N), sense=po.minimize)
+    model.knownPermutations = po.ConstraintList()
 
-    print('Creating Solver using glpk')
-    solver = po.SolverFactory('glpk')
-    print('Solving using glpk')
-    results = solver.solve(model)
+    id = np.eye(n_nodes)
+    known_result = id
 
-    print(results)
+    found_transformations = []
+
+    for i_result in count(0):
+        print(f'Computing Cycle of Solution P_{i_result}')
+        all_powers = []
+        power = known_result.copy()
+        while True:
+            all_powers.append(power)
+            power = power@known_result
+            if np.allclose(power, id):
+                break
+
+        found_transformations.append(all_powers)
+
+        print('Creating Constraints to Exclude Known Solution and its Powers')
+        for p, power in enumerate(all_powers):
+            print(f'P_{i_result}^{p}')
+            model.knownPermutations.add(expr=sum(model.P[tuple(ij)] for ij in np.argwhere(power)) <= n_nodes - 1)
+
+        print('Solving using glpk')
+        results = solver.solve(model, tee=True)
+        print(results)
+
+        known_result = to_ndarray(model.P, n_nodes, n_nodes)
+        print(f'P_{i_result + 1} =')
+        matshow(known_result)
 
     #print('Creating Solver using MindtPy')
     #solver = po.SolverFactory('mindtpy')
     #print('Solving using glpk and ipopt')
     #solver.solve(model, mip_solver='glpk', nlp_solver='ipopt')
-
-    print(to_ndarray(model.P, n_nodes, n_nodes))
 
 
 if __name__ == '__main__':
