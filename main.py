65b5dccf5898eace5017798549408c3f908aa36a
Aleksandar Vučković <aleksandar.vuckovic@merckgroup.com>
2021-11-16 11:09:01 +0100

    Added printing of the matrix filename.

--- main.py
+++ main.py
@@ -196,7 +196,9 @@ def find_permutations(A: np.ndarray, norm: Norm, objective_bound=100, track_time
 
 
 if __name__ == '__main__':
-    with open('data/one_letter_words_5x5_concurrence_matrix_100.pickle', 'rb') as f:
+    filename = 'data/one_letter_words_5x5_concurrence_matrix_100.pickle'
+    print(f'Loading file {filename}')
+    with open(filename, 'rb') as f:
         A = pickle.load(f)
 
     find_permutations(A=A, norm=Norm.L1, objective_bound=0.01, track_time=True, glpk_time_limit=180)
