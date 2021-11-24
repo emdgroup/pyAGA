35cf47a5034dcb6acbbc0556635dd19712b47551
Mathias Winkel <mathias.winkel@merckgroup.com>
2021-11-24 09:44:03 +0100

    start splitting into separate functions

--- mipsym/mip.py
+++ mipsym/mip.py
@@ -16,6 +16,7 @@ from mipsym import scip  # noqa: F401
 
 
 logger = logging.getLogger('integer_programming')
+logger.setLevel(level=logging.DEBUG)  # set to logging.INFO for less, to logging.DEBUG for more verbosity
 
 
 class Norm(Enum):
@@ -36,6 +37,79 @@ def hash_array(arr: np.ndarray) -> Tuple[int]:
     # This is unique for each permuation matrix and can be used
     # e.g. as a key in a dict
     return tuple(np.nonzero(arr.flatten())[0])
+
+
+def create_mip_model(norm: Norm, A: np.ndarray):
+
+    assert A.ndim == 2
+    assert A.shape[0] == A.shape[1]
+    assert isinstance(norm, Norm)
+
+    n_nodes = A.shape[0]
+
+    start_time = time.time()
+
+    model = po.ConcreteModel()
+    model.N = po.Set(initialize=range(n_nodes))
+
+    logger.debug('Creating Parameter for Concurrence Matrix')
+    model.A = po.Param(model.N, model.N, initialize=lambda m, i, j: A[i, j], within=po.Reals)
+
+    logger.debug('Creating Boolean Permutation Matrix')
+    model.P = po.Var(model.N, model.N, within=po.Boolean)
+
+    logger.debug('Creating Row Sum Constraint for the Permutation Matrix')
+    model.rowSum = po.Constraint(model.N, rule=lambda m, i: 1 == sum(m.P[i, j] for j in m.N))
+    logger.debug('Creating Column Sum Constraint for the Permutation Matrix')
+    model.colSum = po.Constraint(model.N, rule=lambda m, j: 1 == sum(m.P[i, j] for i in m.N))
+
+    logger.debug('Creating Constraint to Exclude Identity')
+    model.identityConstraint = po.Constraint(expr=sum(model.P[i, i] for i in range(n_nodes)) <= n_nodes - 1)
+
+    def deviation(m, i, j):
+        return sum(m.P[i, k]*m.A[k, j] - m.A[i, k]*m.P[k, j] for k in m.N)
+
+    if norm == Norm.L_INFINITY:
+        logger.debug('Creating Upper Limit Variable for the Maximum Error')
+        model.T = po.Var(within=po.NonNegativeReals)
+
+        logger.debug('Creating Objective Function to Minimize L_INFINITY Norm of Deviation')
+        model.objective = po.Objective(expr=model.T, sense=po.minimize)
+
+        logger.debug('Creating Constraint to Limit Positive Deviation')
+        model.posDev = po.Constraint(model.N, model.N, rule=lambda m, i, j: deviation(m, i, j) <= m.T)
+        logger.debug('Creating Constraint to Limit Negative Deviation')
+        model.negDev = po.Constraint(model.N, model.N, rule=lambda m, i, j: -m.T <= deviation(m, i, j))
+    elif norm == Norm.L_1:
+        logger.debug('Creating Upper Limit Variables for the Pointwise Error')
+        model.T = po.Var(model.N, model.N, within=po.NonNegativeReals)
+
+        logger.debug('Creating Objective Function to Minimize L_1 Norm of Deviation')
+        model.objective = po.Objective(expr=sum(model.T[i, j] for j in model.N for i in model.N), sense=po.minimize)
+
+        logger.debug('Creating Constraint to Limit Positive Deviation')
+        model.posDev = po.Constraint(model.N, model.N, rule=lambda m, i, j: deviation(m, i, j) <= m.T[i, j])
+        logger.debug('Creating Constraint to Limit Negative Deviation')
+        model.negDev = po.Constraint(model.N, model.N, rule=lambda m, i, j: -m.T[i, j] <= deviation(m, i, j))
+    elif norm == Norm.L_2:
+        logger.debug('Creating Objective Function to Minimize L_2 Norm of Deviation')
+        model.objective = po.Objective(rule=lambda m: sum(deviation(m, i, j)**2 for j in m.N for i in m.N), sense=po.minimize)
+    else:
+        raise ValueError(f'Unsupported Norm {norm}.')
+
+    logger.debug(f'Finished creation of model in {time.time()-start_time:.2f} seconds.')
+
+    return model
+
+
+def fix_known_entries(model, known_entries):
+    for index, value in enumerate(known_entries):
+        if value is not None:
+            for i in model.N:
+                model.P[i, value].fix(0)
+                model.P[index, i].fix(0)
+            model.P[index, value].fix(1)
+    logger.info('Constrained P =\n' + matshow_pyomo(model.P, model.N, model.N))
 
 
 # TODO: This can probably be optimized later by using SymPy Permutations
@@ -73,11 +147,6 @@ def find_permutations(A: np.ndarray, norm: Norm, solver: Solver = Solver.GLPK, o
     # assert isinstance(norm, Norm)
 
     n_nodes = A.shape[0]
-
-    start_time = time.time()
-
-    model = po.ConcreteModel()
-    model.N = po.Set(initialize=range(n_nodes))
 
     if solver == Solver.GLPK:
         solver_factory_params = dict(_name='glpk')
@@ -121,63 +190,13 @@ def find_permutations(A: np.ndarray, norm: Norm, solver: Solver = Solver.GLPK, o
 
     ip_solver.options = solver_options
 
-    logger.debug('Creating Parameter for Concurrence Matrix')
-    model.A = po.Param(model.N, model.N, initialize=lambda m, i, j: A[i, j], within=po.Reals)
-
-    logger.debug('Creating Boolean Permutation Matrix')
-    model.P = po.Var(model.N, model.N, within=po.Boolean)
-
+    model = create_mip_model(norm, A)
     if known_entries is not None:
-        for index, value in enumerate(known_entries):
-            if value is not None:
-                for i in model.N:
-                    model.P[i, value].fix(0)
-                    model.P[index, i].fix(0)
-                model.P[index, value].fix(1)
-        logger.info('Constrained P =\n' + matshow_pyomo(model.P, model.N, model.N))
+        fix_known_entries(model, known_entries)
 
-    logger.debug('Creating Row Sum Constraint for the Permutation Matrix')
-    model.rowSum = po.Constraint(model.N, rule=lambda m, i: 1 == sum(m.P[i, j] for j in m.N))
-    logger.debug('Creating Column Sum Constraint for the Permutation Matrix')
-    model.colSum = po.Constraint(model.N, rule=lambda m, j: 1 == sum(m.P[i, j] for i in m.N))
-
-    logger.debug('Creating Constraint to Exclude Identity')
-    model.identityConstraint = po.Constraint(expr=sum(model.P[i, i] for i in range(n_nodes)) <= n_nodes - 1)
-
-    def deviation(m, i, j):
-        return sum(m.P[i, k]*m.A[k, j] - m.A[i, k]*m.P[k, j] for k in m.N)
-
-    if norm == Norm.L_INFINITY:
-        logger.debug('Creating Upper Limit Variable for the Maximum Error')
-        model.T = po.Var(within=po.NonNegativeReals)
-
-        logger.debug('Creating Objective Function to Minimize L_INFINITY Norm of Deviation')
-        model.objective = po.Objective(expr=model.T, sense=po.minimize)
-
-        logger.debug('Creating Constraint to Limit Positive Deviation')
-        model.posDev = po.Constraint(model.N, model.N, rule=lambda m, i, j: deviation(m, i, j) <= m.T)
-        logger.debug('Creating Constraint to Limit Negative Deviation')
-        model.negDev = po.Constraint(model.N, model.N, rule=lambda m, i, j: -m.T <= deviation(m, i, j))
-    elif norm == Norm.L_1:
-        logger.debug('Creating Upper Limit Variables for the Pointwise Error')
-        model.T = po.Var(model.N, model.N, within=po.NonNegativeReals)
-
-        logger.debug('Creating Objective Function to Minimize L_1 Norm of Deviation')
-        model.objective = po.Objective(expr=sum(model.T[i, j] for j in model.N for i in model.N), sense=po.minimize)
-
-        logger.debug('Creating Constraint to Limit Positive Deviation')
-        model.posDev = po.Constraint(model.N, model.N, rule=lambda m, i, j: deviation(m, i, j) <= m.T[i, j])
-        logger.debug('Creating Constraint to Limit Negative Deviation')
-        model.negDev = po.Constraint(model.N, model.N, rule=lambda m, i, j: -m.T[i, j] <= deviation(m, i, j))
-    elif norm == Norm.L_2:
+    if norm == Norm.L_2:
         assert solver in (Solver.IPOPT, Solver.SCIP)
         ip_solver.set_problem_format(ProblemFormat.mps)
-        logger.debug('Creating Objective Function to Minimize L_2 Norm of Deviation')
-        model.objective = po.Objective(rule=lambda m: sum(deviation(m, i, j)**2 for j in m.N for i in m.N), sense=po.minimize)
-    else:
-        raise ValueError(f'Unsupported Norm {norm}.')
-
-    logger.debug(f'Finished creation of model in {time.time()-start_time:.2f} seconds.')
 
     id = np.eye(n_nodes, dtype=int)
 
