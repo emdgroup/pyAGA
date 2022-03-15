c1e59890085f56d15d49e3b70d22bf8a603f3e32
Aleksandar Vučković <aleksandar.vuckovic@merckgroup.com>
2022-03-15 12:23:37 +0100

    Ran black linter on main_integer_program.py .

--- main_integer_program.py
+++ main_integer_program.py
@@ -3,15 +3,17 @@ import logging
 
 from mipsym.mip import find_permutations, Norm, Solver
 
-logging.basicConfig(format='%(asctime)s [%(levelname)s] %(message)s')
-logger = logging.getLogger('integer_programming')
-logger.setLevel(level=logging.INFO)  # set to logging.INFO for less, to logging.DEBUG for more verbosity
+logging.basicConfig(format="%(asctime)s [%(levelname)s] %(message)s")
+logger = logging.getLogger("integer_programming")
+logger.setLevel(
+    level=logging.INFO
+)  # set to logging.INFO for less, to logging.DEBUG for more verbosity
 
 
-if __name__ == '__main__':
-    filename = 'data/one_letter_words_5x5_integers_concurrence_matrix_100.pickle'
-    logger.info(f'Loading file {filename}')
-    with open(filename, 'rb') as f:
+if __name__ == "__main__":
+    filename = "data/one_letter_words_5x5_integers_concurrence_matrix_100.pickle"
+    logger.info(f"Loading file {filename}")
+    with open(filename, "rb") as f:
         A = pickle.load(f)
 
     find_permutations(
@@ -20,5 +22,5 @@ if __name__ == '__main__':
         solver=Solver.SCIP,
         objective_bound=0.01,
         time_limit=None,
-        known_entries=None
+        known_entries=None,
     )
