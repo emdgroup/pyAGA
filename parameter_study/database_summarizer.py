689735b44bcaaae9d36e01010b769d11eef81d85
Aleksandar Vučković <aleksandar.vuckovic@merckgroup.com>
2022-04-20 09:31:47 +0200

    database_summarizer.py: Added plotting of results.

--- parameter_study/database_summarizer.py
+++ parameter_study/database_summarizer.py
@@ -12,7 +12,7 @@ testcase = "13x7_letters_indiv_colors"
 # testcase = "11x7_letters_indiv_colors"
 regex_str = f"{testcase}.*xlsx"
 expected_permutation_group_order = 1092
-
+plot = False
 sub_directory = "jobarray_13x7_combined"
 
 dataframes = []
@@ -38,3 +38,29 @@ big_df = big_df.sort_values(by=sorting)
 
 os.makedirs(f"results/{sub_directory}/summaries", exist_ok=True)
 big_df.to_excel(f"results/{sub_directory}/summaries/{testcase}_summary_everything.xlsx")
+
+if plot:
+    status_files_started = []
+    runtime_labels = {}
+
+    status_files_finished = []
+    fig, ax = plt.subplots()
+    correctness_colors = {
+        "exact": "green",
+        "too many": "black",
+        "too few": "red",
+        "exact (Timeout)": "purple",
+        "too few (Timeout)": "pink",
+    }
+    for correctness, color in correctness_colors.items():
+        # plot exact points
+        df = big_df[big_df["is_group_order_correct"] == correctness]
+        ax.plot(
+            df["percentage_observation"],
+            df["trafo_fault_tolerance_ratio"],
+            color=color,
+            linestyle="None",
+            marker="o",
+        )
+    plt.show()
+    fig.savefig("13x7_evl=0_005.pdf")
